4bd4bd646519cb1f75a1bc2993af5179f1a5fc73
xiaop0817 <orc_xiaop@163.com>
2021-08-17 15:55:55 +0800

    add mongo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xiaop0817/ftgoutils
 
 go 1.16
+
+require gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
---
+++ mongo/mongo.go
@@ -0,0 +1,186 @@
+package mongo
+
+import (
+	"fmt"
+	"github.com/xiaop0817/ftgoutils/c"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"time"
+)
+
+var Session *mgo.Session
+
+func InitMongo(host string, db string, usrName string, pwd string) {
+	dialInfo := &mgo.DialInfo{
+		Addrs:     []string{host},
+		Timeout:   60 * time.Second,
+		Source:    db,
+		Username:  usrName,
+		Password:  pwd,
+		PoolLimit: 4096,
+	}
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+			fmt.Println(c.C(".............Mongo连接失败..............", c.LightYellow))
+			time.Sleep(time.Second * 4)
+		}
+	}()
+
+	s, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		log.Fatalf("Mongo Create Session: %s\n", err)
+	}
+	Session = s
+}
+
+// 获取文档对象
+func connect(db string, collection string) (*mgo.Session, *mgo.Collection) {
+	if Session != nil {
+		s := Session.Copy()
+		c := s.DB(db).C(collection)
+		s.SetMode(mgo.Monotonic, true)
+		return s, c
+	}
+	return nil, nil
+}
+
+// Insert 插入数据
+// db string 操作的数据库
+// collection string 操作的文档(表)
+// docs 数据
+func Insert(db, collection string, docs ...interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Insert(docs...)
+}
+
+// FindOne 查询单条数据
+// db string 操作的数据库
+// collection string 操作的文档(表)
+// query:查询条件
+// selector:需要过滤的数据(projection)
+// result:查询到的结果
+func FindOne(db, collection string, query, selector, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Select(selector).One(result)
+}
+
+// FindAll 查询数据列表
+// db string 操作的数据库
+// collection string 操作的文档(表)
+// query:查询条件
+// selector:需要过滤的数据(projection)
+// result:查询到的结果
+func FindAll(db, collection string, query, selector, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Find(query).Select(selector).All(result)
+}
+
+// FindAllSort 查询数据列表
+// db string 操作的数据库
+// collection string 操作的文档(表)
+// query:查询条件
+// selector:需要过滤的数据(projection)
+// result:查询到的结果
+// sort:排序 exam:(-create_time,+index)
+func FindAllSort(db, collection string, query, selector, result interface{}, sort ...string) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Find(query).Sort(sort...).Select(selector).All(result)
+}
+
+// UpdateOne 更新一条数据
+// db string 操作的数据库
+// collection string 操作的文档(表)
+// query:要更新的文档id
+// docs:需要更新的文档数据
+// result:查询到的结果
+func UpdateOne(db, collection string, id bson.ObjectId, docs interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	//ObjectIdHex
+	return c.Update(bson.M{"_id": id}, docs)
+}
+
+func Update(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Update(selector, update)
+}
+
+func Count(db, collection string, query interface{}) (int, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Count()
+}
+
+func GroupBy(db, collection string, query interface{}) (int, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Count()
+}
+
+// BulkInsert 批量插入记录
+// db string 操作的数据库
+// collection string 操作的文档(表)
+// docs:需要写入的记录列表
+func BulkInsert(db, collection string, docs ...interface{}) (*mgo.BulkResult, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	bulk := c.Bulk()
+	bulk.Insert(docs...)
+	return bulk.Run()
+}
+
+// RemoveAll 删除符合条件的全部记录
+// db string 操作的数据库
+// collection string 操作的文档(表)
+// selector 记录条件
+func RemoveAll(db, collection string, selector interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	_, err := c.RemoveAll(selector)
+	return err
+}
+
+func BulkRemove(db, collection string, selector ...interface{}) (*mgo.BulkResult, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	bulk := c.Bulk()
+	bulk.Remove(selector...)
+	return bulk.Run()
+}
+
+// Upsert 插入或更新
+// db string 操作的数据库
+// collection string 操作的文档(表)
+// update 需要写入的记录
+func Upsert(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	_, err := c.Upsert(selector, update)
+	return err
+}
+
+func GetAndUpdate(db, collection string) int {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	change := mgo.Change{
+		Update:    bson.M{"$inc": bson.M{"seq": 1}},
+		Upsert:    true,
+		ReturnNew: true,
+	}
+	doc := struct{ Seq int }{}
+	c.Find(bson.M{"_id": "order_seq_" + time.Now().Format("20060102")}).Apply(change, &doc)
+	return doc.Seq
+}
